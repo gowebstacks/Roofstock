61f12c598e2d80ba766139843a9c1acd6d7c264b
Anton <anton@nikandigital.com>
2019-12-17 21:18:43 +0200

    @crazypinetree

---
+++ js/main.js
@@ -0,0 +1,38 @@
+$(document).ready(function ($) {
+
+
+
+
+
+    // var mySwiper = new Swiper('#reviews', {
+    //     speed: 500,
+    //     spaceBetween: 30,
+    //     slidesPerView: 3,
+    //     centeredSlides: true,
+    //     initialSlide: 1,
+    //     loop: true
+    // });
+    //
+    // if ($(window).width() < 991) {
+    //     $('#header .btn-green').appendTo('.slicknav_nav li:last-child');
+    // }
+    //
+    // var mySwiper2 = new Swiper('#logos-bar', {
+    //     speed: 800,
+    //     loop: true,
+    //     slidesPerView: 4,
+    //     autoplay: true,
+    //     delay: 3500
+    // });
+    //
+    //
+    // $('.btn').click(function(e){
+    //      var offsetTop =$(href).offset().top;
+    //     $('html, body').stop().animate({
+    //         scrollTop: offsetTop
+    //     }, 1000);
+    //     return false
+    //
+    // });
+});
+
